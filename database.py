d510cc11cd7d658ef63763cd34abf4a19ecd7ad6
Samanvitha U <ullengalasamanvitha1855@gmail.com>
2026-01-22 21:25:02 +0530

    Postgres ready

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/ai_medical_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
